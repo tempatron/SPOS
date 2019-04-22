6d4520474ace8d7c8a0a282860243f16214aa98a
Abhinav <abhinavraut420@gmail.com>
2019-04-22 21:39:04 +0530

    added scheduling algorithm

---
+++ src/com/tempatron/FCFS.java
@@ -0,0 +1,82 @@
+package com.tempatron;
+
+import java.util.Scanner;
+
+public class FCFS {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number  of process: ");
+        int n = sc.nextInt();
+        int[] pid = new int[n];   // process ids
+        int[] ar = new int[n];     // arrival times
+        int[] bt = new int[n];     // burst or execution times
+        int[] ct = new int[n];     // completion times
+        int[] ta = new int[n];     // turn around times
+        int[] wt = new int[n];     // waiting times
+        int temp;
+        float avgwt = 0, avgta = 0;
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("enter process " + (i + 1) + " arrival time : ");
+            ar[i] = sc.nextInt();
+            System.out.println("enter process " + (i + 1) + " burst time : ");
+            bt[i] = sc.nextInt();
+            pid[i] = i + 1;
+        }
+
+        //sorting according to arrival times
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - (i + 1); j++) {
+                if (ar[j] > ar[j + 1]) {
+                    swap(ar, j, j + 1);
+                    swap(bt, j, j + 1);
+                    swap(pid, j, j + 1);
+//                    temp = ar[j];
+////                    ar[j] = ar[j+1];
+////                    ar[j+1] = temp;
+////                    temp = bt[j];
+////                    bt[j] = bt[j+1];
+////                    bt[j+1] = temp;
+////                    temp = pid[j];
+////                    pid[j] = pid[j+1];
+////                    pid[j+1] = temp;
+                }
+            }
+        }
+
+        // finding completion times
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                ct[i] = ar[i] + bt[i];
+            } else {
+                if (ar[i] > ct[i - 1]) {
+                    ct[i] = ar[i] + bt[i];
+                } else
+                    ct[i] = ct[i - 1] + bt[i];
+            }
+            ta[i] = ct[i] - ar[i];           // turnaround time = completion time- arrival time
+            wt[i] = ta[i] - bt[i];          // waiting time = turnaround time- burst time
+            avgwt += wt[i];                // total waiting time
+            avgta += ta[i];               // total turnaround time
+        }
+
+        System.out.println("\npid  arrival  burst  complete turn waiting");
+        for (int i = 0; i < n; i++) {
+            System.out.println(pid[i] + "  \t\t " + ar[i] + "\t\t" + bt[i] + "\t\t" + ct[i] + "\t\t" + ta[i] + "\t\t" + wt[i]);
+        }
+        sc.close();
+        System.out.println("\nAverage waiting time : " + (avgwt / n));     // printing average waiting time.
+        System.out.println("Average turnaround time : " + (avgta / n));    // printing average turnaround time.
+    }
+
+    private static void swap(int[] array, int i, int j) {
+        if (i == j) {
+            return;
+        } else {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+
+}
